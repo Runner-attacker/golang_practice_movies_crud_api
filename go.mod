a7cbf00d80a4421ee4b327e5c8dba4a08d13e306
Sahil-Bhisma <bhisma.koirala@bidhee.com>
2025-07-31 15:28:10 +0545

    Added Get,Create and Delete Function

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/Runner-attacker/GO-Movies-Crud
 
 go 1.22.2
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require github.com/gorilla/mux v1.8.1
--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -19,14 +22,51 @@ type Director struct {
 	LastName  string `json:"lastname"`
 }
 
-var movie []Movie
+var movies []Movie
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range movies {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+		}
+	}
+}
+
+func createMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(1000000))
+	movies = append(movies, movie)
+}
 
 func main() {
+
 	r := mux.NewRouter()
+	movies = append(movies, Movie{ID: "1", Isbn: "4568", Title: "Movie 1", Director: &Director{FirstName: "Sahil", LastName: "koirala"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "2058", Title: "Movie 2", Director: &Director{FirstName: "Kaushika", LastName: "Koirala"}})
+	movies = append(movies, Movie{ID: "3", Isbn: "7872", Title: "Movie 3", Director: &Director{FirstName: "Dwarika", LastName: "Koirala"}})
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovies).Methods("POST")
-	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	// r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
 	r.HandleFunc("/movie/{id}", deleteMovie).Methods("DELETE")
 
 	fmt.Printf("Starting server at port 8000")
