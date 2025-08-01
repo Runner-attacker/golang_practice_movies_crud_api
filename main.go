2f20bc73b8bff2606cfa9445d307e09ea6c77977
Sahil-Bhisma <bhisma.koirala@bidhee.com>
2025-08-01 19:45:38 +0545

    added update function

--- main.go
+++ main.go
@@ -57,6 +57,22 @@ func createMovies(w http.ResponseWriter, r *http.Request) {
 	movies = append(movies, movie)
 }
 
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
+	}
+}
+
 func main() {
 
 	r := mux.NewRouter()
@@ -66,7 +82,7 @@ func main() {
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovies).Methods("POST")
-	// r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
 	r.HandleFunc("/movie/{id}", deleteMovie).Methods("DELETE")
 
 	fmt.Printf("Starting server at port 8000")
